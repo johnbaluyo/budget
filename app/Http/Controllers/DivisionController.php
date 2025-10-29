b128646d1230776ed00db35c3d4b8aa4d0c9bd4e
johnbaluyo <johnbaluyo@gmail.com>
2025-10-29 17:10:31 +0800

    route fix 2

--- app/Http/Controllers/DivisionController.php
+++ app/Http/Controllers/DivisionController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class DivisionController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $my_data = Division::all();
